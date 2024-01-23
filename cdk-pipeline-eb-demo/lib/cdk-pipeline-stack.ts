032fa62f0be309f7b91c0e6e91928c9dea7fe81a
eseantatum <eseantatum@gmail.com>
2024-01-23 14:53:32 -0500

    new app

--- cdk-pipeline-eb-demo/lib/cdk-pipeline-stack.ts
+++ cdk-pipeline-eb-demo/lib/cdk-pipeline-stack.ts
@@ -22,8 +22,9 @@ export class CdkPipelineStack extends Stack {
          // Install dependencies, build and run cdk synth
          //installCommands: ['npm i -g npm@latest'],
          commands: [
-          'cd cdk-pipeline-eb-demo/src', 
-          'docker build -t helloworld . && docker run -p 8080:3000 -it -t helloworld'
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
          ],
        }),
     });
